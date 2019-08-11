dc686f96f5a31f8c5c7ab32d0bb884fe9efa766a
Ron Ganguly <c1ph3r9@gmail.com>
2019-08-11 03:06:30 -0500

    broken homepage into smaller component and finished styling menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Homepage from './homepage.component';
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<HomePage />
 		</div>
 	);
 }
