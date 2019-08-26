126629d6e6e4ddf65e8ebe76eea035d8b72248d6
Ron Ganguly <c1ph3r9@gmail.com>
2019-08-26 00:36:03 -0500

    add redux to project

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { auth } from '../../firebase/firebase.utils';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 const Header = ({ currentUser }) => (
 	<div className='header'>
@@ -27,4 +28,7 @@ const Header = ({ currentUser }) => (
 		</div>
 	</div>
 );
-export default Header;
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+});
+export default connect(mapStateToProps)(Header);
